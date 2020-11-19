9c1115d6114d206d81f99e7088a404895d524a15
Robert Ernst <r.f.ernst-3@umcutrecht.nl>
2020-11-19 09:37:42 +0100

    Add subtract

--- example.py
+++ example.py
@@ -1,7 +1,14 @@
 def add(a, b):
     return a + b
 
+def subtract(a, b):
+    return a - b
 
 def test_add():
     assert add(2, 3) == 5
+    assert add(2, 3.5) == 5.5
     assert add("space", "ship") == "spaceship"
+
+def test_subtract():
+    assert subtract(2, 3) == -1
+    assert subtract(3, 2.5) == 0.5
